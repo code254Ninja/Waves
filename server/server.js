343ffbe31672a96be403e27ca9698df13bd52ef4
SimonKihara <kimcode254@gmail.com>
2020-01-08 13:23:27 +0300

    Basic setting of the server is complete, User and products routes are complete

--- server/server.js
+++ server/server.js
@@ -40,26 +40,40 @@ const { auth } =require('./middleware/auth');
 const { admin } =require('./middleware/admin');
 
 
-//PRODUCTS ROUTES
-app.post('/api/product/article', (req,res)=>{
-    const product = new Product(req.body);
-    product.save((err,doc)=>{
-        if(err) return res.json({success:false,err});
-        res.status(200).json({
-            success:true,
-            product:doc
-        })
+//=================================
+//             PRODUCTS
+//=================================
+
+// BY ARRIVAL
+// /articles?sortBy=createdAt&order=desc&limit=4
+
+// BY SELL
+// /articles?sortBy=sold&order=desc&limit=100&skip=5
+app.get('/api/product/articles',(req,res)=>{
+
+    let order = req.query.order ? req.query.order : 'asc';
+    let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
+    let limit = req.query.limit ? parseInt(req.query.limit) : 100;
+
+    Product.
+    find().
+    populate('brand').
+    populate('wood').
+    sort([[sortBy,order]]).
+    limit(limit).
+    exec((err,articles)=>{
+        if(err) return res.status(400).send(err);
+        res.send(articles)
     })
 })
 
 
-//PRODUCTBYID
-
-app.get('/api/product/article_by_id',(req,res)=>{
+/// /api/product/article?id=HSHSHSKSK,JSJSJSJS,SDSDHHSHDS,JSJJSDJ&type=single
+app.get('/api/product/articles_by_id',(req,res)=>{
     let type = req.query.type;
     let items = req.query.id;
 
-    if(type == "array"){
+    if(type === "array"){
         let ids = req.query.id.split(',');
         items = [];
         items = ids.map(item=>{
@@ -68,53 +82,73 @@ app.get('/api/product/article_by_id',(req,res)=>{
     }
 
     Product.
-    find({'_id':{$in:items}}).
+    find({ '_id':{$in:items}}).
+    populate('brand').
+    populate('wood').
     exec((err,docs)=>{
-        return res.status(200).send(docs);
-        
+        return res.status(200).send(docs)
+    })
+});
+
+
+app.post('/api/product/article',auth,admin,(req,res)=>{
+    const product = new Product(req.body);
+
+    product.save((err,doc)=>{
+        if(err) return res.json({success:false,err});
+        res.status(200).json({
+            success: true,
+            article: doc
+        })
     })
 })
 
-//WOODS ROUTES
+//=================================
+//              WOODS
+//=================================
+
 app.post('/api/product/wood',auth,admin,(req,res)=>{
     const wood = new Wood(req.body);
 
     wood.save((err,doc)=>{
-        if(err) return res.json({success:false, err})
+        if(err) return res.json({success:false,err});
         res.status(200).json({
-            success:true,
-            wood:doc
+            success: true,
+            wood: doc
         })
     })
-})
+});
 
 app.get('/api/product/woods',(req,res)=>{
     Wood.find({},(err,woods)=>{
         if(err) return res.status(400).send(err);
-        res.status(200).send(woods);
+        res.status(200).send(woods)
     })
 })
 
-//BRANDS ROUTE
+
+//=================================
+//              BRAND
+//=================================
+
 app.post('/api/product/brand',auth,admin,(req,res)=>{
     const brand = new Brand(req.body);
 
     brand.save((err,doc)=>{
-        if(err) return res.json({success:false,err})
+        if(err) return res.json({success:false,err});
         res.status(200).json({
             success:true,
-            brand:doc
+            brand: doc
         })
     })
 })
 
-app.get('/api/product/brands', (req,res)=>{
+app.get('/api/product/brands',(req,res)=>{
     Brand.find({},(err,brands)=>{
         if(err) return res.status(400).send(err);
-        res.status(200).send(brands);
+        res.status(200).send(brands)
     })
 })
-
 
 //USERS ROUTE
 app.get('/api/users/auth', auth, (req,res)=>{

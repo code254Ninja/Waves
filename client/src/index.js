5c970e5aeddbb50a30e25d19714793ddbfeef5d4
SimonKihara <kimcode254@gmail.com>
2020-01-31 10:43:20 +0300

    Redux

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,24 @@ import "./Resources/css/styles.css";
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware} from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+
+import Reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, applyMiddleware)(createStore);
+
 
 ReactDOM.render(
+<Provider store = { createStoreWithMiddleware(Reducers, 
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__())}>
 <BrowserRouter>
 <Routes />
 </BrowserRouter>
+</Provider>
+
 , document.getElementById('root'));
 
 
